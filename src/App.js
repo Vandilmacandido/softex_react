ef113290eda5820757e0bd30ea236253fab27d13
Vandilma Candido <90811919+Vandilmacandido@users.noreply.github.com>
2022-12-22 09:38:22 -0300

    git.add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+function App() {
+  return (
+    <div className="App">
+      <h1>Vandilma Candido</h1>
+      <Button title = "Entrar"/>
+      <Button title = "Fechar"/>
+      <Button title = "Salvar"/>
+      <Button title = "Sair"/>
+
+    </div>
+  );
+}
+
+export default App;
